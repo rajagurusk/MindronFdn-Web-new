ed95eaae42c45b764e9a851d47d62e908edacab5
rajagurusk <rajagurusivakumar1@example.com>
2026-03-21 11:35:57 +0530

    CHANGES

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
-const API_URL =
-  import.meta.env.VITE_API_URL?.trim() || "http://localhost:10000";
+const API_URL = import.meta.env.VITE_API_URL?.trim();
+
+console.log("Frontend API URL:", API_URL);
 
 export default API_URL;
